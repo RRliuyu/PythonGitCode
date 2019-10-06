0fa389031778cd59ded675dd30407274769b06b8
RRliuyu <37231406+RRliuyu@users.noreply.github.com>
2019-10-06 10:51:57 +0800

    Delete test2.py

--- test2.py
+++
@@ -1,6 +0,0 @@
-
-import tushare as ts
-import datetime
-api = ts.pro_api('5f37df276dab4fff9a3783499fd18174ec34caf4a84dc1e953c7d987')
-df = api.cashflow(ts_code='000938.SZ', start_date='20180101', end_date='20180730')
-print(df)
